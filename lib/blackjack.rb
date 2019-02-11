d61e2507b4f9d95e2c0610075648aa16543315ee
Ben Manekin <ben.manekin@gmail.com>
2019-02-11 19:50:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,11 +66,11 @@ end
 def runner
   # code runner here
   card_total = 0
-  welcome
-  initial_round
-  hit?
-  display_card_total
   until card_total > 21
+    welcome
+    initial_round
+    hit?
+    display_card_total
   
 end
     
